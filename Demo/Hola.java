d5b62d126b110e11c72ef1ea9d4261b354c3acc0
MACS-KINCAID <mitsiu.carreno@upa.edu.mx>
2025-11-26 08:20:15 -0600

    saludar dinamico

--- Demo/Hola.java
+++ Demo/Hola.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 class Hola {
   public static void main(String[] args) throws Exception{
-    System.out.println("Hola");
+    Saludar.saludar();
   }
 }
 
+class Saludar{
+  static String obtenerNombre(){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Ingresa tu nombre: ");
+    String nombre = s.nextLine();
+    return nombre;
+  }
+
+  static void saludar(){
+    String nombre = obtenerNombre();
+    System.out.println("Hola " + nombre);
+  }
+}
