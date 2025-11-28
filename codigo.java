9685594a516956ec1a88a0efe1f1893d6791d069
ManuelDLRS <up240399@alumnos.upa.edu.mx>
2025-11-28 09:07:53 -0600

    Agregar lo faltante

--- codigo.java
+++ codigo.java
@@ -1,5 +1,5 @@
 public class codigo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("Esta rama es de Manuel Arturo De La Rosa Moreno");
 	}
 }
