e8deebbd4ff48a4506f49ea5ac62d252167cbf19
Yael Aketzali <up240842@alumnos.upa.edu.mx>
2025-11-28 08:44:40 -0600

    Primer demo pr

---
+++ Yaya/SumaNumeros.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sumaNumeros {
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Ingresa el primer número: ");
+        int numero1 = entrada.nextInt();
+
+        System.out.print("Ingresa el segundo número: ");
+        int numero2 = entrada.nextInt();
+
+        int resultado = numero1 + numero2;
+
+        System.out.println("La suma de " + numero1 + " + " + numero2 + " es: " + resultado);
+
+        entrada.close();
+    }
+}
