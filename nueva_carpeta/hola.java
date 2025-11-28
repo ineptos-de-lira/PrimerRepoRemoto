ba9a61d26e809a6a132db3d70e009d2446b6a8c8
ManuelMG <up240460@alumnos.upa.edu.mx>
2025-11-28 08:19:59 -0600

    Primer demo

---
+++ nueva_carpeta/hola.java
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+public class hola {
+	public static void main (String[] args) {
+		Scanner lector = new Scanner(Sytem.in);
+		System.out.prinln("Ingresa tu edad: ");
+		int edad = lector.nextInt();
+	}
+		if (edad => 18) {
+			System.out.println("Puedes comprar cheves!")
+		} else {
+		  Sytem.out.println("Todavia no puedes comprar cheves")
+		}
+}
+
