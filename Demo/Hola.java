e4088f080b84f0f99f6c17ad81246d86262d6bb3
GabrielaMoralesR <up240055@alumnos.upa.edu.mx>
2025-11-28 08:23:52 -0600

    para aprender gabs

---
+++ Demo/Hola.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Hola {
+  public static void main(String[] args) throws Exception{
+    Saludar.saludar();
+  }
+}
+
+class Saludar{
+  static String obtenerNombre(){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Ingresa tu nombre: ");
+    String nombre = s.nextLine();
+    s.close();
+    return nombre;
+  }
+
+  static void saludar(){
+    String nombre = obtenerNombre();
+    System.out.println("Hola " + nombre);
+  }
+}
