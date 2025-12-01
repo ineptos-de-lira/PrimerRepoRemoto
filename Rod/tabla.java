421ee5e5295e688f5ebb4d20e4b0c95d89388613
Rodrigo <up240232@alumnos.upa.edu.mx>
2025-12-01 07:43:50 -0600

    Se agregaron comentarios

--- Rod/tabla.java
+++ Rod/tabla.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class tabla {
     public static void main(String[] args) {
+        //Creacion de Scanner
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa el numero de la tabla que deseas crear: ");
         int n = sc.nextInt();
